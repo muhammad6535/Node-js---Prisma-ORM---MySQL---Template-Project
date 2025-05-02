b0ee08dccc4c38059d835d78940a712da1756a73
muhammad6535 <mohmed6535@gmail.com>
2025-05-02 16:48:43 +0300

    some updates

---
+++ prisma/migrations/20250502134358_my_migration/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(10) NOT NULL,
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    UNIQUE INDEX `user_phone_key`(`phone`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
